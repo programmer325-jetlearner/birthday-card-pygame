a2ff8dc8b8d4399cd2ff6cf6fea7a680a66f69d9
programmer325-jetlearner <asyedtanim1@gmail.com>
2025-09-21 11:49:52 +0100

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,34 @@
+import pygame
+import time
+
+pygame.init()
+
+WIDTH=600
+HEIGHT=600
+
+
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("good luck card")
+
+img=pygame.image.load("bg.jpg")
+image=pygame.transform.scale(img,(600,600))
+running=True
+while running:
+    font=pygame.font.SysFont("Arial",70)
+    text1=font.render("GOOD",True,("blue"))
+    text2=font.render("LUCK",True,("blue"))
+    screen.fill((255,255,255))
+    screen.blit(image,(0,0))
+    screen.blit(text1,(180,-10))
+    screen.blit(text2,(180,60))
+    pygame.display.update()
+    time.sleep(2)
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+            pygame.quit()
+            exit()
+
+pygame.quit()
+        
